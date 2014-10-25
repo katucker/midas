38628cfd617e61723e3e13756f6c4be9517ada09
Keith Tucker <keith.tucker@hhs.gov>
2014-10-25 15:22:48 -0400

    Resolve home page differences.
    Edited home page settings in require configuration.

--- assets/js/backbone/config/require_config.js
+++ assets/js/backbone/config/require_config.js
@@ -78,13 +78,6 @@ require.config({
     'footer_template'           : '../apps/footer/templates/footer_template.html',
 	
     // ----------
-    //= Home
-    // ----------
-    'home_view'               : '../apps/home/views/home_view',
-    'home_template'           : '../apps/home/templates/home_template.html',
-    'home_controller'          : '../apps/home/controllers/home_controller',
-	
-    // ----------
     //= About
     // ----------
     'about_view'               : '../apps/about/views/about_view',
